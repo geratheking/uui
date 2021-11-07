93d366a16c68bbca1bb6d27942d246ff06231ca1
Gera <anton.gerasymenko@gmail.com>
2021-11-07 20:15:24 +0200

    fix isAdmin middleware

--- index.ts
+++ index.ts
@@ -2,14 +2,12 @@ import App from './src/app';
 import config from './config';
 import bodyParser from "koa-bodyparser";
 import errorHandler from './src/middlewares/errorHandler';
-import isAdmin from "./src/middlewares/isAdmin";
 
 const app = new App(
     config.port,
     [
         bodyParser(),
         errorHandler,
-        isAdmin,
     ]
 );
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
+import isAdmin from "./middlewares/isAdmin";
 import usersController from './controllers/usersController'
 
 const initRoutes = (router: Router): void => {
     router.get('/users', usersController.getItems);
-    router.post('/users', usersController.createItem);
-    router.delete('/users/:itemId', usersController.deleteItem);
+    router.post('/users', isAdmin, usersController.createItem);
+    router.delete('/users/:itemId', isAdmin, usersController.deleteItem);
 }
 
 export default initRoutes;
